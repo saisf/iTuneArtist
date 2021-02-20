c94d86207640f8da1f9ba9d1b7291de932bd45e5
Sai Leung <sleung82@mail.ccsf.edu>
2021-02-20 15:37:40 -0800

    Initial Commit

---
+++ iTuneArtist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTuneArtist
+//
+//  Created by Sai Leung on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
